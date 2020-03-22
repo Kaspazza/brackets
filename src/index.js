b7da56cd14b96be09262b648f847cf167e03839f
Kaspazza <kaspazza@gmail.com>
2020-03-23 00:26:44 +0100

    refactored code

--- src/index.js
+++ src/index.js
@@ -3,28 +3,42 @@ module.exports = function check(str, bracketsConfig) {
 
     for(const letter of str){
         const currentConfig = bracketsConfig.find(conf => conf.includes(letter));
-        const stackLen = stack.length;
+
         if(currentConfig === undefined){
             return false;
         } else if(currentConfig[0] === currentConfig[1]){
-            if(stackLen === 0){
-                stack.push(letter);
-            } else if(stack[stackLen-1] === currentConfig[0]){
-                stack.pop();
-            } else {
-                stack.push(letter);
-            }
+            handleEqualStartAndEndConfig(stack, currentConfig, letter);
         } else if(currentConfig[0] === letter) {
             stack.push(letter);
         } else {
-            if(stackLen === 0) {
-                return false;
-            } else if(stack[stackLen-1] === currentConfig[0]) {
-                stack.pop();
+            if(isClosingBracketCorrect(stack, currentConfig[0])){
+                stack.pop()
             } else {
                 return false;
             }
         }
     }
     return stack.length === 0;
+}
+
+function handleEqualStartAndEndConfig(stack, currentConfig, letter){
+    const stackLen = stack.length;
+    if(stackLen === 0){
+        stack.push(letter);
+    } else if(stack[stackLen-1] === currentConfig[0]){
+        stack.pop();
+    } else {
+        stack.push(letter);
+    }
+}
+
+function isClosingBracketCorrect(stack, currentConfigOpeningLetter) {
+    const stackLen = stack.length;
+    if(stackLen === 0) {
+        return false;
+    } else if(stack[stackLen-1] === currentConfigOpeningLetter) {
+        return true;
+    } else {
+        return false;
+    }
 }
